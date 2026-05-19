234d4cc9d49408e91f303ecefff8605c5ee52e89
Dahfaith <iamdahfaith@gmail.com>
2026-05-19 07:34:01 -0700

    script.js

--- script.js
+++ script.js
@@ -157,7 +157,7 @@ function updateCartUI() {
 
 function executeCartCheckout() {
     if (basketCart.length === 0) { return; }
-    const baseNumber = "2348143644489";
+    const baseNumber = "2348073733419";
     let orderSummary = "Hello Omoolopaase Empire, I want to purchase the following spiritual solutions:\n\n";
     let grandTotal = 0;
     basketCart.forEach((item, index) => {
@@ -172,7 +172,7 @@ function executeCartCheckout() {
 }
 
 function bookConsultation(tierType) {
-    const baseNumber = "2348143644489";
+    const baseNumber = "2348073733419";
     let msg = "Hello, I am ready for the Consultation. I want to select the normal one.";
     if (tierType === 'vip') { msg = "Hello, I am ready for the Consultation. I would love to sign up for the VIP one."; }
     else if (tierType === 'face-to-face') { msg = "Hello, I am ready for the Consultation. I want to book a physical face-to-face session."; }
@@ -208,8 +208,8 @@ function changeCurrency(currency) {
 }
 
 /* --- SALES TOAST NOTIFICATIONS --- */
-const salesLocations = ["Lagos, Nigeria", "London, UK", "Abuja, Nigeria", "Atlanta, USA", "Houston, USA", "Abeokuta, Ogun State", "Port Harcourt", "Ibadan, Nigeria", "Toronto, Canada", "Kano, Nigeria", "Dubai, UAE", "Lekki, Lagos"];
-const salesProducts = ["Cash Out Soap", "Stay With Me Mirror", "Breakthrough Soap", "VIP Consultation", "Atude Soap", "Eyonu Agba", "Fuck & Pay", "Glory Retrieval", "Open Door Kit"];
+const salesLocations = ["Lagos, Nigeria", "London, UK", "Abuja, Nigeria", "Atlanta, USA", "Houston, USA", "Abeokuta, Ogun State", "Ogbomosho, Nigeria" "Port Harcourt", "Ibadan, Nigeria", "Toronto, Canada", "Kano, Nigeria", "Dubai, UAE", "Lekki, Lagos"];
+const salesProducts = ["Cash Out Soap", "Stay With Me Mirror", "Curse Breaker Soap" "Breakthrough Soap", "VIP Consultation", "Atude Soap", "Eyonu Agba", "Fuck & Pay", "Glory Retrieval", "Open Door Kit"];
 
 function showSalesToast() {
     const toast = document.getElementById('salesToast');
